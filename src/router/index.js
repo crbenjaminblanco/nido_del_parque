f43a5a8b4b71e0cd16531391398aa6571e3aa051
Benjamín Blanco <crbenjaminblanco@gmail.com>
2025-05-04 00:48:55 -0600

    Update to hash

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import i18n from '../i18n/i18n'
 
@@ -46,7 +46,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
   scrollBehavior(to, from, savedPosition) {
     if (to.params.section) {
