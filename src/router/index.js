d95fb0db93fd91a1ccca19caf8becd1cb6cb860c
Benjamín Blanco <crbenjaminblanco@gmail.com>
2025-05-03 23:57:58 -0600

    use history hash

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 // Function to get user's preferred language
@@ -41,7 +41,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
   scrollBehavior(to, from, savedPosition) {
     if (to.params.section) {
